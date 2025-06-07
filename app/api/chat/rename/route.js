e38bc92bdb046a64a5d2870e1aa675e4a129b933
aashihs maurya <aashishmaurya959@gmail.com>
2025-06-07 23:11:27 +0530

    add

--- app/api/chat/rename/route.js
+++ app/api/chat/rename/route.js
@@ -1,26 +1,28 @@
-
 import connectDB from "@/config/db";
 import Chat from "@/models/Chat";
-import { auth } from "@clerk/nextjs";  // Updated import
+import { auth } from "@clerk/nextjs"; // Updated import
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-    try {
-        const { userId } = auth();  // Updated usage
+  try {
+    const { userId } = auth(); // Updated usage
 
-        if(!userId){
-            return NextResponse.json({ success: false, message: "User not authenticated", })
-        }
-
-        // get chatId & name from request `req`
-        const { chatId, name} = await req.json();
-
-        // connect to the database and update the chat name
-        await connectDB();
-        await Chat.findOneAndUpdate({ _id: chatId, userId}, { name });
-
-        return NextResponse.json({ success: true, message: "Chat Renamed!!" });
-    } catch (error) {
-        return NextResponse.json({ success: false, error: error.message });
+    if (!userId) {
+      return NextResponse.json({
+        success: false,
+        message: "User not authenticated",
+      });
     }
+
+    // get chatId & name from request `req`
+    const { chatId, name } = await req.json();
+
+    // connect to the database and update the chat name
+    await connectDB();
+    await Chat.findOneAndUpdate({ _id: chatId, userId }, { name });
+
+    return NextResponse.json({ success: true, message: "Chat Renamed!!" });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message });
+  }
 }
